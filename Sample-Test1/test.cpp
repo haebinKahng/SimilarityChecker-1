86d1d9c8d2ac820ceea4c3b223772f0e1c103bb2
Minyoung Kang <haebin1223.myk@gmail.com>
2024-05-31 10:12:26 +0900

    length 추가

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,5 +1,23 @@
 #include "pch.h"
+#include "../SimilarityChecker-1/length.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
+class SimilarityCheckerFixture : public testing::Test {
+public:
+	SimilarityCheck game{ "ABCDE" };
+};
+
+TEST_F(SimilarityCheckerFixture, lengthcheck1) {
+	EXPECT_EQ(60, game.getScore("AAAAA"));
+}
+
+TEST_F(SimilarityCheckerFixture, lengthcheck2) {
+	EXPECT_EQ(0, game.getScore("AA"));
+}
+
+TEST_F(SimilarityCheckerFixture, lengthcheck3) {
+	EXPECT_EQ(0, game.getScore("AAAAAAAAAA"));
+}
+
+TEST_F(SimilarityCheckerFixture, lengthcheck4) {
+	EXPECT_EQ(20, game.getScore("ABC"));
 }
